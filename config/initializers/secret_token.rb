2d0c213174b0f763f09d2ac21dac307a2d76c9ff
Harrison Woods <hbwoodsjr@gmail.com>
2014-03-14 02:15:22 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EasyJobs::Application.config.secret_key_base = 'e32b782f4d8473a0893283af7f5c375ac66714c50269e6a2aa27c3fd4493c0f78ed8f573fd19c3eaed5ced987718e3dd90248f8e058dc66d6180fccd9258d699'
